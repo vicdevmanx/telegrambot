a4c437b7d8cb5536415ee3cf65e54d2928d05536
vicdevmanx <vicdevmanx@gmail.com>
2025-06-01 14:32:10 +0100

    refactor: improve bot.js structure and add dotenv dependency

--- bot.js
+++ bot.js
@@ -4,14 +4,18 @@ import axios from "axios"
 const app = express()
 const PORT = 8080
 
+//middleware
+app.use(express.json())
+
 app.get("/", (req, res) => {
-    res.status(200).json({message: "welcome to vicassistant bot!"})
+    res.status(200).json({ message: "welcome to vicassistant bot!" })
 })
 
-
+app.post("/webhook/telegram", (req, res) => {
+    console.log(req)
+    res.status(200).send('OK');
+})
 
 app.listen(PORT, () => {
-console.log(`bot running on 127.0.0.1:${PORT}`)
+    console.log(`bot running on 127.0.0.1:${PORT}`)
 });
-
-export default app
