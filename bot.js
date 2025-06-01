d292072d85511ac0e178782a1b416ed3d0d37ff0
vicdevmanx <vicdevmanx@gmail.com>
2025-06-01 14:57:13 +0100

    refactor: enhance bot.js structure and improve webhook handling

--- bot.js
+++ bot.js
@@ -1,18 +1,29 @@
 import express from "express"
 import axios from "axios"
+import dotenv from 'dotenv';
 
-const app = express()
-const PORT = 8080
+
+const app = express();
+const PORT = 8080;
+dotenv.config();
 
 //middleware
 app.use(express.json())
+
+//controllers
+const sendMsg = (id, msg) => {
+    axios.post(`${process.env.TELEGRAM_BASE_URL}/sendMessage`, {chat_id: id, text: `you said ${msg}`})
+}
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "welcome to vicassistant bot!" })
 })
 
-app.post("/webhook/telegram", (req, res) => {
-    console.log(req)
+app.post("/webhook/telegram", async (req, res) => {
+    const {ok, result } = req.body
+    const id = result[result.length-1].message.from.id
+    const msg = result[result.length-1].message.text
+    sendMsg(id, msg)
     res.status(200).send('OK');
 })
 
